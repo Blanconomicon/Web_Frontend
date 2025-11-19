3b72948fa52fb983400bd94d4fe5c353104f81b5
Daniel Alvarez Burgo <danielalvarezburgo@gmail.com>
2025-11-19 23:48:20 +0100

    Estructura de archivos

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Aketza Gonzalez Rey">
+    <meta name="author" content="Daniel Alvarez Burgo">
+    <meta name="description" content="Pagina principal en la cual, se muestra una breve descripcion de que es Blanconomicon">
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="icon" href="./src/img/logo1.png" type="image/x-icon">
+    <title>Blanconomicon</title>
+</head>
+<body>
+    <header>
+        <h1>Blanconomicon</h1>
+        <nav class="menu">
+            <div class="menu--etiqueta">
+                <a href="index.php"><img src="./src/img/logo2.png" alt="Despliega Menu" class="menu--logo"></a>
+            </div>
+            <div class="menu--items">
+                <!-- Menu items -->
+            </div>
+        </nav>
+    </header>
+    <main>
+        <!-- Informacion de la Pagina -->
+        <section class="contenedor">
+            <img width=300px src="src\img\logo1.png" alt="">
+            <img width=300px src="src\img\logo2.png" alt="">
+        </section>
+    </main>
+    <footer>
+        <!-- Footer -->
+    </footer>
+    <script type="text/javascript" src="./js/cookie.js"></script>
+</body>
+</html>
