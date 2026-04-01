d819d7ef9d78cee0f9e73b3e472244c30c717ab8
FormedNumber95 <aketza200510@gmail.com>
2026-04-01 17:14:26 +0200

    añadidas caracteristicas

--- php/views/verPersonaje.php
+++ php/views/verPersonaje.php
@@ -10,66 +10,96 @@ require_once "../includes/header.php";
 require_once "../includes/footer.php"
 ?>
 <script src="../../js/menuHamburguesa.js"></script>
-  <main class="dnd">
-    <section class="dnd__sheet">
+<main class="dnd">
+  <section class="dnd__sheet">
 
-      <!-- CABECERA -->
-      <div class="dnd__header">
-        <div class="dnd__title">Nombre del personaje</div>
-        <div class="dnd__text">Clase <span class="dnd__muted">- Subclase</span></div>
-        <div class="dnd__text">Raza</div>
-        <div class="dnd__text">Trasfondo</div>
-      </div>
+    <!-- CABECERA -->
+    <div class="dnd__header">
+      <div class="dnd__title">Nombre del personaje</div>
+      <div class="dnd__text">Clase <span class="dnd__muted">- Subclase</span></div>
+      <div class="dnd__text">Raza</div>
+      <div class="dnd__text">Trasfondo</div>
+    </div>
+
+    <!-- STATS PRINCIPALES -->
+    <div class="dnd__stats">
+      <div class="dnd__stat-box">CA</div>
+      <div class="dnd__stat-box">Velocidad</div>
+      <div class="dnd__stat-box">Bonif. competencia</div>
+    </div>
+
+    <!-- ATRIBUTOS + HABILIDADES -->
+    <div class="dnd__columns">
 
       <!-- ATRIBUTOS -->
       <div class="dnd__attributes">
 
         <div class="dnd__attribute">
           <div class="dnd__attr-name">Fuerza</div>
-          <div class="dnd__attr-mod">Modificador</div>
-          <div class="dnd__attr-score">10</div>
-        </div>
-
-        <div class="dnd__attribute">
-          <div class="dnd__attr-name">Destreza</div>
-          <div class="dnd__attr-mod">Modificador</div>
+          <div class="dnd__attr-mod">+2</div>
           <div class="dnd__attr-score">14</div>
         </div>
 
         <div class="dnd__attribute">
+          <div class="dnd__attr-name">Destreza</div>
+          <div class="dnd__attr-mod">+3</div>
+          <div class="dnd__attr-score">16</div>
+        </div>
+
+        <div class="dnd__attribute">
           <div class="dnd__attr-name">Constitución</div>
-          <div class="dnd__attr-mod">Modificador</div>
+          <div class="dnd__attr-mod">+1</div>
           <div class="dnd__attr-score">12</div>
         </div>
 
         <div class="dnd__attribute">
           <div class="dnd__attr-name">Inteligencia</div>
-          <div class="dnd__attr-mod">Modificador</div>
-          <div class="dnd__attr-score">16</div>
+          <div class="dnd__attr-mod">+0</div>
+          <div class="dnd__attr-score">10</div>
         </div>
 
         <div class="dnd__attribute">
           <div class="dnd__attr-name">Sabiduría</div>
-          <div class="dnd__attr-mod">Modificador</div>
-          <div class="dnd__attr-score">11</div>
+          <div class="dnd__attr-mod">+1</div>
+          <div class="dnd__attr-score">12</div>
         </div>
 
         <div class="dnd__attribute">
           <div class="dnd__attr-name">Carisma</div>
-          <div class="dnd__attr-mod">Modificador</div>
-          <div class="dnd__attr-score">13</div>
+          <div class="dnd__attr-mod">+4</div>
+          <div class="dnd__attr-score">18</div>
         </div>
 
       </div>
 
-      <!-- STATS -->
-      <div class="dnd__stats">
-        <div class="dnd__stat-box">CA</div>
-        <div class="dnd__stat-box">Movimiento</div>
-        <div class="dnd__stat-box">Bonificador de competencia</div>
+      <!-- HABILIDADES -->
+      <div class="dnd__skills">
+
+        <div class="dnd__skill"><span>Acrobacias</span><span class="dnd__skill-mod">+3</span></div>
+        <div class="dnd__skill"><span>Arcanos</span><span class="dnd__skill-mod">+2</span></div>
+        <div class="dnd__skill"><span>Atletismo</span><span class="dnd__skill-mod">+2</span></div>
+        <div class="dnd__skill"><span>Engaño</span><span class="dnd__skill-mod">+6</span></div>
+        <div class="dnd__skill"><span>Historia</span><span class="dnd__skill-mod">+2</span></div>
+        <div class="dnd__skill"><span>Interpretación</span><span class="dnd__skill-mod">+6</span></div>
+        <div class="dnd__skill"><span>Intimidación</span><span class="dnd__skill-mod">+4</span></div>
+        <div class="dnd__skill"><span>Investigación</span><span class="dnd__skill-mod">+2</span></div>
+        <div class="dnd__skill"><span>Juego de manos</span><span class="dnd__skill-mod">+5</span></div>
+        <div class="dnd__skill"><span>Medicina</span><span class="dnd__skill-mod">+1</span></div>
+        <div class="dnd__skill"><span>Naturaleza</span><span class="dnd__skill-mod">+2</span></div>
+        <div class="dnd__skill"><span>Percepción</span><span class="dnd__skill-mod">+3</span></div>
+        <div class="dnd__skill"><span>Perspicacia</span><span class="dnd__skill-mod">+3</span></div>
+        <div class="dnd__skill"><span>Persuasión</span><span class="dnd__skill-mod">+6</span></div>
+        <div class="dnd__skill"><span>Religión</span><span class="dnd__skill-mod">+2</span></div>
+        <div class="dnd__skill"><span>Sigilo</span><span class="dnd__skill-mod">+3</span></div>
+        <div class="dnd__skill"><span>Supervivencia</span><span class="dnd__skill-mod">+1</span></div>
+
       </div>
 
-      <!-- BLOQUES -->
+    </div>
+
+    <!-- BLOQUES INFERIORES -->
+    <div class="dnd__bottom">
+
       <div class="dnd__block">
         <div class="dnd__block-title">Equipo</div>
         <div class="dnd__block-content"></div>
@@ -85,8 +115,10 @@ require_once "../includes/footer.php"
         <div class="dnd__block-content"></div>
       </div>
 
-    </section>
-  </main>
+    </div>
+
+  </section>
+</main>
 </body>
 
 </html>
