d8ae64d0b6aeff01236f46cfc8f981ab947e1fb0
FormedNumber95 <aketza200510@gmail.com>
2026-04-05 09:39:17 +0200

    añadidos procedimientos

--- php/utility/conexion.php
+++ php/utility/conexion.php
@@ -1,11 +1,138 @@
 <?php
 //funcion que crea una conexion con la bd y devuelve la conexion o false
-    function conexion(String $ruta,String $nombreDB,String $usuario,String $password){
-        try{
-            $con=new PDO("mysql:host=$ruta;dbname=$nombreDB",$usuario,$password);
-            return $con;
-        }catch(PDOException $e){
-            echo $e->getMessage();
-            return false;
-        }
+function conexion(String $ruta, String $nombreDB, String $usuario, String $password)
+{
+    try {
+        $con = new PDO("mysql:host=$ruta;dbname=$nombreDB", $usuario, $password);
+        return $con;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        return false;
     }
+}
+
+//Funcion para obtener una lista de los backgrounds o la informacion de un background completo
+function getBackground(PDO $con, $id = null)
+{
+    try {
+        $stmt = $con->prepare("CALL getBackground(:id)");
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($resultados as $fila) {
+            print_r($fila);
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+//TODO poner el comentario bien
+function getCharacter(PDO $con, $id = null, $nick = null)
+{
+    try {
+        $stmt = $con->prepare("CALL getCharacter(:id, :nick)");
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->bindParam(":nick", $nick, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+//Funcion para obtener una lista de las clases o la informacion de una clase completo
+function getClass(PDO $con, $id = null)
+{
+    try {
+        $stmt = $con->prepare("CALL getClass(:id)");
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+//TODO poner el comentario
+function getGroup(PDO $con, $id = null, $nick = null)
+{
+    try {
+        $stmt = $con->prepare("CALL getGroup(:id, :nick)");
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->bindParam(":nick", $nick, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+//TODO poner el comentario
+function getGroupMembers(PDO $con, $id = null, $nick = null)
+{
+    try {
+        $stmt = $con->prepare("CALL getGroupMembers(:id, :nick)");
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->bindParam(":nick", $nick, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+//TODO poner el comentario
+function getPass(PDO $con, $nick = null)
+{
+    try {
+        $stmt = $con->prepare("CALL getPass(:nick)");
+        $stmt->bindParam(":nick", $nick, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+//Funcion para obtener una lista de las razas/especies o la informacion de una raza/especie completo
+function getRace(PDO $con, $id = null)
+{
+    try {
+        $stmt = $con->prepare("CALL getrace(:id)");
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+//TODO poner el comentario
+function getUser(PDO $con, $nick)
+{
+    try {
+        $stmt = $con->prepare("CALL getUser(:nick)");
+        $stmt->bindParam(":nick", $nick, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+//TODO poner el comentario
+function putUser(PDO $con, $nick, $username, $mail, $hash, $salt)
+{
+    try {
+        $stmt = $con->prepare("CALL putUser(:nick, :username, :mail, :hash, :salt)");
+        $stmt->bindParam(":nick", $nick, PDO::PARAM_STR);
+        $stmt->bindParam(":username", $username, PDO::PARAM_STR);
+        $stmt->bindParam(":mail", $mail, PDO::PARAM_STR);
+        $stmt->bindParam(":hash", $hash, PDO::PARAM_STR);
+        $stmt->bindParam(":salt", $salt, PDO::PARAM_STR);
+        return $stmt->execute();
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
