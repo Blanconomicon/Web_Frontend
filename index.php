c544c4842676692287d00ab9e9c64a14a655f15d
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2026-04-30 14:09:16 +0200

    actualizada visualizacion de index

--- index.php
+++ index.php
@@ -3,27 +3,30 @@ require_once "./php/utility/utils.php";
 require_once "./php/includes/header.php";
 ?>
 <main>
-    <!-- Informacion de la Pagina -->
-    <section class="contenedor">
+  <!-- Informacion de la Pagina -->
+  <section class="contenedor">
+    <h2 class="centrado">¿Qué es BLANCONOMICON?</h2>
+    <div class="mostrarTextosIndex">
+      <p>
+        Bienvenido a Blanconomicon, una web para crear tus personajes de Dragones y Mazmorras
+        5.5e en castellano, diseñada para acompañarte desde la primera idea hasta la ficha final
+        de tu aventurero. Blanconomicon no solo te permite crear personajes de nivel 1, sino también
+        almacenarlos de forma organizada para que puedas revisarlos y reutilizarlos en cualquier momento,
+        manteniendo siempre a mano el desarrollo completo de tus héroes a lo largo del tiempo.
+      </p>
+      <p>
+        Blanconomicon también permite entrar en grupos con otras personas para asignar los roles de cada
+        uno y facilitar así un mejor funcionamiento dentro de la partida, fomentando una experiencia más
+        coordinada y coherente en la mesa de juego. De esta forma, cada jugador puede tener claro su papel
+        dentro del grupo, ayudando a construir aventuras más equilibradas, dinámicas y memorables, donde la
+        cooperación y la narrativa compartida cobran verdadero protagonismo.
+      </p>
+      <div>
         <img width=300px src="src\img\logo1.png" alt="">
-        <br>
         <img width=300px src="src\img\logo2.png" alt="">
-<p>
-  Bienvenido a Blanconomicon, una web para crear tus personajes de Dragones y Mazmorras
-   5.5e en castellano, diseñada para acompañarte desde la primera idea hasta la ficha final
-    de tu aventurero. Blanconomicon no solo te permite crear personajes de nivel 1, sino también
-     almacenarlos de forma organizada para que puedas revisarlos y reutilizarlos en cualquier momento, 
-     manteniendo siempre a mano el desarrollo completo de tus héroes a lo largo del tiempo.
-</p>
-
-<p>
-  Blanconomicon también permite entrar en grupos con otras personas para asignar los roles de cada 
-  uno y facilitar así un mejor funcionamiento dentro de la partida, fomentando una experiencia más 
-  coordinada y coherente en la mesa de juego. De esta forma, cada jugador puede tener claro su papel 
-  dentro del grupo, ayudando a construir aventuras más equilibradas, dinámicas y memorables, donde la 
-  cooperación y la narrativa compartida cobran verdadero protagonismo.
-</p>
-    </section>
+      </div>
+    </div>
+  </section>
 </main>
 <?php
 require_once "./php/includes/footer.php"
